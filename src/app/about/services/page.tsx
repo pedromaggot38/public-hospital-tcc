571f3dabe494627ed843c783bd3244bb0a0094b5
Pedro Sanches <pedromaggot38@gmail.com>
2024-11-20 00:13:20 -0300

    Tipos de atendimento, imagens adicionadas

--- src/app/about/services/page.tsx
+++ src/app/about/services/page.tsx
@@ -8,7 +8,7 @@ const ServicesAboutPage = () => {
       description:
         "Nosso setor de Urgência e Emergência está preparado para atender prontamente a todas as situações críticas. Contamos com uma equipe de profissionais altamente qualificados e equipamentos modernos para garantir o atendimento rápido e eficaz.",
       image:
-        "https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/emergencia.jpg",
       reverse: false,
     },
     {
@@ -16,7 +16,7 @@ const ServicesAboutPage = () => {
       description:
         "Especialistas em ortopedia estão disponíveis para avaliar e tratar traumas e fraturas. Utilizamos técnicas avançadas de diagnóstico por imagem e tratamentos personalizados para garantir a melhor recuperação possível.",
       image:
-        "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/ortopedista.jpg",
       reverse: true,
     },
     {
@@ -24,7 +24,7 @@ const ServicesAboutPage = () => {
       description:
         "Nossos cirurgiões gerais estão à disposição para consultas e procedimentos cirúrgicos diversos. Desde pequenas intervenções até cirurgias mais complexas, oferecemos um atendimento abrangente e humanizado.",
       image:
-        "https://images.unsplash.com/photo-1597089802212-2058d9ffdaab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/cirurgiao_geral.jpg",
       reverse: false,
     },
     {
@@ -32,7 +32,7 @@ const ServicesAboutPage = () => {
       description:
         "Antes de qualquer procedimento cirúrgico, nossos anestesistas realizam consultas detalhadas para garantir sua segurança e conforto. Avaliamos seu histórico médico e esclarecemos todas as suas dúvidas.",
       image:
-        "https://images.unsplash.com/photo-1603731334692-6b80ad72c1a9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/anestesia.jpg",
       reverse: true,
     },
     {
@@ -40,7 +40,7 @@ const ServicesAboutPage = () => {
       description:
         "Oferecemos exames de radiografia com tecnologia de ponta para diagnósticos precisos e rápidos. Nossos profissionais garantem a realização dos exames com cuidado e precisão.",
       image:
-        "https://images.unsplash.com/photo-1551601651-62da1f6ecbec?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/radiografia.jpg",
       reverse: false,
     },
     {
@@ -48,7 +48,7 @@ const ServicesAboutPage = () => {
       description:
         "Realizamos exames de eletrocardiograma para avaliar a atividade elétrica do coração. Este exame é essencial para o diagnóstico de diversas condições cardíacas.",
       image:
-        "https://images.unsplash.com/photo-1582558776203-1b6cb97ffcbc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/eletrocardiograma-ecg.jpg",
       reverse: true,
     },
     {
@@ -56,7 +56,7 @@ const ServicesAboutPage = () => {
       description:
         "Nosso laboratório está equipado para realizar uma ampla gama de exames laboratoriais. Oferecemos resultados precisos e rápidos, fundamentais para o diagnóstico e monitoramento de condições de saúde.",
       image:
-        "https://images.unsplash.com/photo-1581093588401-c6c501805c1b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/Laboratoriais.jpg",
       reverse: false,
     },
     {
@@ -64,7 +64,7 @@ const ServicesAboutPage = () => {
       description:
         "Oferecemos serviços de exames anatomopatológicos, essenciais para o diagnóstico de diversas condições médicas. Nossos patologistas garantem uma análise detalhada e precisa.",
       image:
-        "https://images.unsplash.com/photo-1603987098495-e7d23008b8d7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/anatomopatologico.png",
       reverse: true,
     },
     {
@@ -72,7 +72,7 @@ const ServicesAboutPage = () => {
       description:
         "O tococardiograma é um exame importante para monitorar a saúde fetal durante a gravidez. Utilizamos equipamentos modernos para acompanhar a frequência cardíaca do bebê.",
       image:
-        "https://images.unsplash.com/photo-1511174511562-5f7f9cfb22e1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+        "/ServicesAboutPage/tococardiografia.jpg",
       reverse: false,
     },
   ];
@@ -109,8 +109,8 @@ const ServicesAboutPage = () => {
             src={section.image}
             className={`h-64 w-full object-cover ${section.reverse ? "sm:rounded-br-[20px]" : "sm:rounded-bl-[20px]"
               }`}
-            width={400}
-            height={400}
+            width={1000}
+            height={1000}
           />
         </section>
       ))}
