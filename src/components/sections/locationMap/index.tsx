39f875142d6eb1eef3cc9f7bc3945414ec4c7829
Pedro Sanches <pedromaggot38@gmail.com>
2024-11-24 23:44:52 -0300

    Adicionado endereço em about

--- src/components/sections/locationMap/index.tsx
+++ src/components/sections/locationMap/index.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LocationMap() {
     return (
         <div>
-            <div className="mb-14 text-center">
+            <div className="mb-8 text-center">
                 <span className="py-1 px-4 bg-blue-100 rounded-full text-xs font-medium text-blue-600 text-center">
                     Localização
                 </span>
@@ -11,9 +14,23 @@ export default function LocationMap() {
                 <p className="text-lg font-normal text-gray-500 max-w-md md:max-w-2xl mx-auto">
                     Verifique nosso endereço diretamente pelo mapa
                 </p>
-                <div className="flex mt-6 justify-center">
+                <div className="flex my-6 justify-center">
                     <div className="w-16 h-1 rounded-full bg-blue-500 inline-flex"></div>
                 </div>
+                <Link href="https://maps.app.goo.gl/MH2taJnWe67AhEcF6"
+                    rel="noreferrer"
+                    target="_blank"
+                >
+                    <Image
+                        src="/svg/location.svg"
+                        alt="localização"
+                        width={25}
+                        height={25}
+                        className="mr-2 inline-block"
+                    />
+                    <span className="text-sm text-gray-700">Avenida José Bonifácio, 382, Centro - Maracaí/SP </span>
+                    <p className="text-sm text-gray-700">CEP: 19840-000</p>
+                </Link>
             </div>
             <div className="px-4 py-16 sm:px-6 lg:px-8 relative w-full max-w-full mx-auto h-[500px]">
                 <iframe
